13e4f840860aa085536f23fb7c0f62784da5d53c
Kayla M Alvarado <kayla.alvarado@hotmail.com>
2023-04-11 16:32:28 -0400

    Set up Google Maps to display on browse page

--- src/app/browse/page.js
+++ src/app/browse/page.js
@@ -1,3 +1,33 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+'use client';
+
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: 40.500979167976396, 
+  lng: -74.44743886129068
+};
+
+function MyComponent() {
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyChXfJ4hhbLu-8k7JDP3H9BojfpR9SvArc"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
+
+export default React.memo(MyComponent)
